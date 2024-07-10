8671a46c26858fbbabab5da89a056cce109f5905
Ilayaraja_Veerakalai <ilayaraja62@gmail.com>
2024-07-10 17:01:09 +0530

    file delete

--- get_pkg_version_and_path.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-which docker 2>&1 1>/dev/nul && {echo "Docker installed" ; echo "Docker package path is $(which docker)"; echo "Doocker version is $(docker -v | cut -d ' ' -f3 | tr -d ',')" || echo "Docker not installed"
-
-echo "============================================"
-
