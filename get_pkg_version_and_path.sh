63cd7a16e72f8859b67f47746741a5507739e35a
Ilayaraja_Veerakalai <ilayaraja62@gmail.com>
2024-07-10 16:54:31 +0530

    update

--- get_pkg_version_and_path.sh
+++ get_pkg_version_and_path.sh
@@ -2,3 +2,4 @@
 which docker 2>&1 1>/dev/nul && {echo "Docker installed" ; echo "Docker package path is $(which docker)"; echo "Doocker version is $(docker -v | cut -d ' ' -f3 | tr -d ',')" || echo "Docker not installed"
 
 echo "============================================"
+
